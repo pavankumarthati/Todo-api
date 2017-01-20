6b759fe42c8d2135b1e6921e867aae499b87c69c
pavan kumar <pavankumar.thati@gmail.com>
2017-01-20 09:57:22 +0530

    added todo API

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+var todos = [{
+		id: 1,
+		description: 'Meet mon for lunch',
+		completed: false
+	},
+	{
+		id: 2,
+		description: 'Goto market',
+		completed: false
+	},
+	{
+		id: 3,
+		description: 'start for play',
+		completed: true
+	}];
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
+});
+
+//GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
+	todos.forEach(function (todo) {
+		if (todo.id === todoId) {
+			matchedTodo = todo;
+		}
+    });
+
+	if  (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function() {
