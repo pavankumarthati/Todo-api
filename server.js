e27b920e4582289d3c458adb1381c75e35f59347
pavan kumar <pavankumar.thati@gmail.com>
2017-01-20 21:39:36 +0530

    refactored api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
@@ -17,13 +19,7 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-	todos.forEach(function (todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-		}
-    });
-
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 	if  (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
@@ -32,8 +28,13 @@ app.get('/todos/:id', function (req, res) {
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'completed', 'description');
 
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || _.isEmpty(body.description)) {
+		return res.status(400).send("Bad Request");
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
 	todos.push(body);
