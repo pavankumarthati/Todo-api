54b1f3db6042fbd1666c8a8df588c29105012649
pavan kumar <pavankumar.thati@gmail.com>
2017-01-20 20:48:39 +0530

    add POST method to todos

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
-var todos = [{
-		id: 1,
-		description: 'Meet mon for lunch',
-		completed: false
-	},
-	{
-		id: 2,
-		description: 'Goto market',
-		completed: false
-	},
-	{
-		id: 3,
-		description: 'start for play',
-		completed: true
-	}];
+var todos = [];
+var todoNextId = 1;
+
+var app = express();
+app.use(bodyParser.json());
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
@@ -39,6 +29,17 @@ app.get('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).send();
 	}
+});
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId;
+	todoNextId++;
+	todos.push(body);
+
+	console.log(body);
+	res.send(body);
 });
 
 app.listen(PORT, function() {
